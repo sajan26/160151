4ab844c4485f9ab17e9a5aade6fbeda2456bbf0d
sajan26 <sajan9848>
2016-12-13 09:51:01 +0900

    Aloha.java作りました

---
+++ 20161213/Aloha.java
@@ -0,0 +1,15 @@
+public class Aloha{
+	public static void main(String[] args) {
+		for (int i=0;i<20;i++){
+		
+			if (i%4==0){
+	System.out.println("Aloha");
+			}else{
+				System.out.println("HAPPY NEW YEAR");
+
+			}
+		}
+		System.out.println("1");
+		System.out.println("3");
+	   }
+	}
